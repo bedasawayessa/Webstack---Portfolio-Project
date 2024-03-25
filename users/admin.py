1573230b0a1a9683100ceca3dae77f3189639848
Bedasa <48552699+bedasawayessa@users.noreply.github.com>
2024-03-25 16:13:34 +0300

    Delete users directory

--- users/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from users.models import CustomUser
-
-# Register your models here.
-@admin.register(CustomUser)
-class PatientAdmin(admin.ModelAdmin):
-    list_display = ['username', 'email', 'role', 'is_staff', 'is_admin', 'is_superuser','is_active']
-    search_fields = ['role']
-    list_filter = ['role', 'username']
