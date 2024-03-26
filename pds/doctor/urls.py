66dc67453dea5a56ab014cd51d54a962157adee4
Bedasa <48552699+bedasawayessa@users.noreply.github.com>
2024-03-26 15:25:36 +0300

    doctor-committed

--- pds/doctor/urls.py
+++ pds/doctor/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('doctors/', views.doctor_list, name='doctor_list'),
     path('register_doctor/', views.register_doctor, name='register_doctor'),
     path('doctor/<int:pk>/', views.doctor_detail, name='doctor_detail'),
+    path('doctor/<int:pk>/update/', views.doctor_update, name='doctor_update'),
+    
     
     # Add more paths for creating, updating, and deleting appointments
 ]
--- pds/doctor/views.py
+++ pds/doctor/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Doctor
 from .forms import DoctorForm, SearchForm
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 
 
@@ -18,15 +19,57 @@ def register_doctor(request):
                 return doctor_list(request)  # Redirect to a page showing the list of doctors
         else:
             form = DoctorForm(user_role=user_role)
-        return render(request, 'doctor/register_doctor.html', {'form': form})
+        return render(request, 'doctor/doctor_form.html', {'form': form})
     else:
         return redirect('login_user')  # Redirect unauthorized users to the login page
+
+
+@login_required
+def doctor_update(request, pk):
+    doctor = get_object_or_404(Doctor, pk=pk)
+    
+    # Check if the user is an admin
+    if request.user.is_authenticated and request.user.is_admin:
+        # Admin can update all doctors
+        if request.method == 'POST':
+            form = DoctorForm(request.POST, instance=doctor)
+            if form.is_valid():
+                form.save()
+                return doctor_list(request)
+        else:
+            form = DoctorForm(instance=doctor)
+    else:
+        # Check if the user is a doctor and can only update their own profile
+        if request.user.is_authenticated and request.user.role == 'doctor' and doctor.user == request.user:
+            if request.method == 'POST':
+                form = DoctorForm(request.POST, instance=doctor)
+                if form.is_valid():
+                    form.save()
+                    return redirect('doctor:doctor_detail', pk=pk)  # Redirect to the doctor detail page
+            else:
+                form = DoctorForm(instance=doctor)
+        else:
+            messages.error(request, "You can only edit your own profile.")
+            return doctor_list(request)
+            #return redirect('users:login_user')
+
+    return render(request, 'doctor/doctor_form.html', {'form': form})
+# def doctor_update(request, pk):
+#     doctor = get_object_or_404(Doctor, pk=pk)
+#     if request.method == 'POST':
+#         form = DoctorForm(request.POST, instance=doctor)
+#         if form.is_valid():
+#             form.save()
+#             return doctor_list(request)
+#     else:
+#         form = DoctorForm(instance=doctor)
+#     return render(request, 'doctor/doctor_form.html', {'form': form})
 
 
 # def doctor_list(request):
 #     doctors = Doctor.objects.all()
 #     return render(request, 'doctor/doctor_list.html', {'doctors': doctors})
-
+@login_required
 def doctor_list(request):
     search_form = SearchForm(request.GET)
     doctors = Doctor.objects.all().order_by('-id')
@@ -50,7 +93,7 @@ def doctor_list(request):
     }
 
     return render(request, 'doctor/doctor_list.html', context)
-
+@login_required
 def doctor_detail(request, pk):
     doctor = get_object_or_404(Doctor, pk=pk)
     return render(request, 'doctor/doctor_detail.html', {'doctor': doctor})
